433272b7a511cf52c2da11393e4ebcf508adf8a5
Shreya Singh <shreyasingh.r@ascendion.com>
2026-02-12 17:49:43 +0530

    Updated server

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://story-scene.vercel.app/"
+}));
 app.use(express.json());
 
 const openai = new OpenAI({
@@ -86,7 +89,10 @@ ${story}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
-
